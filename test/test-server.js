30f78b2b60a56a6285d2c08b79ab7cdfe0ee37bc
Kinuyo Fujikawa <kinuyofujikawa@gmail.com>
2017-01-25 11:48:28 -0800

    had duplicate files for test-server.js; removed duplicate

--- test/test-server.js
+++ test/test-server.js
@@ -8,7 +8,7 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 //=================BLOG TEST===================//
-describe("blog-posts", function(){
+describe("/blog-posts", function(){
 	// Activate server before tests run
 	before(function(){
 		return runServer();
@@ -36,10 +36,53 @@ describe("blog-posts", function(){
 					item.should.include.keys(expectedKeys);
 				});
 			});
-	});//END GET TEST
+	});//End GET test
 
 	//Test POST request
-	it("should add a blog post item on POST", function(){
-		const newItem = 
+	it("should add a new blog post item on POST", function(){
+		const newItem = {
+			title: "Another Blog Post", 
+			content: "Content for this blog post", 
+			author:"Arya Stark", 
+			publishDate: "1/25/2017"
+		};
+		return chai.request(app)
+			.post("/blog-posts")
+			.send(newItem)
+			.then(function(res){
+				res.should.have.status(201);
+				res.should.be.json;
+				res.body.should.be.a("object");
+				res.body.should.include.keys("title", "content", "author", "publishDate");
+				res.body.title.should.not.be.null;
+				res.body.should.deep.equal(Object.assign(newItem, {title: res.body.title}));
+			});
+	});//End POST test
+
+	//Test PUT request
+	it("should update blog post on PUT", function(){
+		const updateData = {
+			title: "foo"
+		};
+
+		return chai.request(app)
+			.get("/blog-posts")
+			.then(function(res){
+				updateData.id = res.body[0].id;
+				return chai.request(app)
+					.put(`/blog-posts/${updateData.id}`)
+					.send(updateData);
+			})
+			.then(function(res){
+				res.should.have.status(204);
+			});
+	});//End PUT test
+	//Test DELETE request
+	it("should delete blog post on DELETE", function(){
+		return chai.request(app)
+			.delete(`/blog-posts/${res.body[0].id}`);
 	})
-}) //END DESCRIBE BLOG
+	.then(function(res){
+		res.should.have.status(204);
+	});
+}); //End describe blog
